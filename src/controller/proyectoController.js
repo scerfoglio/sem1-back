789757d6c2e075e5f25428ff213877efaea0a57d
Christian <christian.digiorno@tiendanube.com>
2022-05-25 16:48:23 -0300

    Filtros para GET de proyectos

--- src/controller/proyectoController.js
+++ src/controller/proyectoController.js
@@ -29,6 +29,19 @@ exports.add = function(req,res) {
 
 
 exports.list = async function(req,res) {
-    let proyectos = await Proyecto.find();
+    const normalFilterFields = ['area', 'campo_accion'];
+    const regexFilterFields = ['descripcion'];
+    const filters = {};
+    for (const filterField of normalFilterFields) {
+        if (req.query[filterField]) {
+            filters[filterField] = req.query[filterField];
+        }
+    }
+    for (const filterField of regexFilterFields) {
+        if (req.query[filterField]) {
+            filters[filterField] = { $regex: new RegExp(req.query[filterField], 'i') };
+        }
+    }
+    const proyectos = await Proyecto.find(filters);
     res.json(proyectos)
 }
